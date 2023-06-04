61c7786eba6e6b04f14101249612cdeb1f8bc0e2
Becker <gb611137@fh-muenster.de>
2023-06-04 13:15:40 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -23,7 +23,7 @@ with st.sidebar:
 
 source3 = source.copy()
 source3 = source3.drop(columns="Datum")
-for i in source3.columns():
+for i in source3.columns:
     source3[i] = source3[i].astype(float)
 table = source.sum(axis=1)
 st.table(data=table)
