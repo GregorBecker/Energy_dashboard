1385a86cf62cbeafd545c9e205c19b0d23fdb96d
Becker <gb611137@fh-muenster.de>
2023-06-04 13:04:34 +0200

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -21,94 +21,86 @@ with st.sidebar:
     st.button("Lade neue Daten", on_click=main.create_new_zappi_data(date))
     
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Netzfrequenzmessung an der Wallbox",
-                                  "Spannungsmessung an der Wallbox",
-                                  "Energiebezugsmessung durch Wallbox",
-                                  "PV-Produktion",
-                                  "Diff"])
+# plot net frequency
+st.altair_chart(
+    alt.Chart(source).mark_line().encode(
+        x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
+        y=alt.Y('Netzfrequenz in Hz',
+                scale=alt.Scale(domain=[np.min(source["Netzfrequenz in Hz"]),
+                                        np.max(source["Netzfrequenz in Hz"])]))
+    ).properties(width="container"),
+    use_container_width=True)
 
-with tab1:
-    # plot net frequency
-    st.altair_chart(
-        alt.Chart(source).mark_line().encode(
-            x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-            y=alt.Y('Netzfrequenz in Hz',
-                    scale=alt.Scale(domain=[np.min(source["Netzfrequenz in Hz"]),
-                                            np.max(source["Netzfrequenz in Hz"])]))
-        ).properties(width="container"),
-        use_container_width=True)
-
-with tab2:
-    st.altair_chart(
-        alt.Chart(source).mark_line().encode(
-            x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-            y=alt.Y('Spannung Phase 1 in V',
-                    scale=alt.Scale(domain=[np.min(source["Spannung Phase 1 in V"]),
-                                            np.max(source["Spannung Phase 1 in V"])]
-                                    ))
-        ).properties(width="container"),
-        use_container_width=True)
-    
-with tab3:
-    st.altair_chart(
-        alt.Chart(source).mark_line().encode(
-            x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-            y=alt.Y('Import',
-                    scale=alt.Scale(domain=[np.min(source["Import"]),
-                                            np.max(source["Import"])]
-                                    ))
-        ).properties(width="container"),
-        use_container_width=True)
-    
-with tab4:
-    st.altair_chart(
-            alt.Chart(source2).mark_line().encode(
-                    x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-                    y=alt.Y('Ertrag in kWh',
-                            scale=alt.Scale(
-                                domain=[np.min(source2["Ertrag in kWh"]),
-                                        np.max(source2["Ertrag in kWh"])]
+st.altair_chart(
+    alt.Chart(source).mark_line().encode(
+        x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
+        y=alt.Y('Spannung Phase 1 in V',
+                scale=alt.Scale(domain=[np.min(source["Spannung Phase 1 in V"]),
+                                        np.max(source["Spannung Phase 1 in V"])]
                                 ))
-            ).properties(width="container"),
-            use_container_width=True)
+    ).properties(width="container"),
+    use_container_width=True)
+    
 
-with tab5:
-    plt1 = alt.Chart(source).mark_line().encode(
-            x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-            y=alt.Y('Diff_Import',
-                    scale=alt.Scale(domain=[np.min(source["Diff_Import"]),
-                                            np.max(source["Diff_Import"])]
-                                    ))
-        )
-    plt2 = alt.Chart(source).mark_line().encode(
+st.altair_chart(
+    alt.Chart(source).mark_line().encode(
+        x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
+        y=alt.Y('Import',
+                scale=alt.Scale(domain=[np.min(source["Import"]),
+                                        np.max(source["Import"])]
+                                ))
+    ).properties(width="container"),
+    use_container_width=True)
+    
+
+st.altair_chart(
+        alt.Chart(source2).mark_line().encode(
                 x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
-                y=alt.Y('Diff_Export',
-                        scale=alt.Scale(domain=[np.min(source["Diff_Export"]),
-                                                np.max(source["Diff_Export"])]
-                                        ))
-        )
-    
-    st.altair_chart(plt1 + plt2, use_container_width=True)
+                y=alt.Y('Ertrag in kWh',
+                        scale=alt.Scale(
+                            domain=[np.min(source2["Ertrag in kWh"]),
+                                    np.max(source2["Ertrag in kWh"])]
+                            ))
+        ).properties(width="container"),
+        use_container_width=True)
 
-    bar = alt.Chart(source).mark_area(opacity=0.7, interpolate='cardinal').encode(
-            x=alt.X('Datum', axis=alt.Axis(title='Datum')),
-            y=alt.Y('pos. Leistung Phase 2 in kW'),
-            tooltip=['Datum', alt.Tooltip('pos. Leistung Phase 2 in kW')],
-            fill=alt.Fill(field='Type', title='Courbe : '))
 
-    bar_original = alt.Chart(source).mark_line(color='red',
-                                               interpolate='cardinal',
-                                               opacity=0.7).encode(
-            x=alt.X('Datum', axis=alt.Axis(title='Datum')),
-            y=alt.Y('Energieimport in kWh'),
-            tooltip=['Datum', alt.Tooltip('Energieimport in kWh')],
-            opacity=alt.Opacity(field='Type', title=None))
+plt1 = alt.Chart(source).mark_line().encode(
+        x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
+        y=alt.Y('Diff_Import',
+                scale=alt.Scale(domain=[np.min(source["Diff_Import"]),
+                                        np.max(source["Diff_Import"])]
+                                ))
+    )
+plt2 = alt.Chart(source).mark_line().encode(
+            x=alt.X('Datum', axis=alt.Axis(labelAngle=-20)),
+            y=alt.Y('Diff_Export',
+                    scale=alt.Scale(domain=[np.min(source["Diff_Export"]),
+                                            np.max(source["Diff_Export"])]
+                                    ))
+    )
 
-    st.altair_chart(bar + bar_original, use_container_width=True)
-    
-    #st.area_chart(data=source,
-    #              x="Datum",
-    #              y=["Leistung Phase 1 in kW",
-    #                 "Leistung Phase 2 in kW",
-    #                 "Leistung Phase 3 in kW"],
-    #              )
+st.altair_chart(plt1 + plt2, use_container_width=True)
+
+bar = alt.Chart(source).mark_area(opacity=0.7, interpolate='cardinal').encode(
+        x=alt.X('Datum', axis=alt.Axis(title='Datum')),
+        y=alt.Y('pos. Leistung Phase 2 in kW'),
+        tooltip=['Datum', alt.Tooltip('pos. Leistung Phase 2 in kW')],
+        fill=alt.Fill(field='Type', title='Courbe : '))
+
+bar_original = alt.Chart(source).mark_line(color='red',
+                                           interpolate='cardinal',
+                                           opacity=0.7).encode(
+        x=alt.X('Datum', axis=alt.Axis(title='Datum')),
+        y=alt.Y('Energieimport in kWh'),
+        tooltip=['Datum', alt.Tooltip('Energieimport in kWh')],
+        opacity=alt.Opacity(field='Type', title=None))
+
+st.altair_chart(bar + bar_original, use_container_width=True)
+
+#st.area_chart(data=source,
+#              x="Datum",
+#              y=["Leistung Phase 1 in kW",
+#                 "Leistung Phase 2 in kW",
+#                 "Leistung Phase 3 in kW"],
+#              )
